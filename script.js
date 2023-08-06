fb009ea8e0524b4ba327e12904bf6dde56fb521d
PepeLasagna <tiagoapkp@gmail.com>
2023-08-06 12:15:59 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+
+
+
+let boton = document.getElementById("contactar");
+let contacto = document.getElementById("contacto");
+function none(){
+    contacto.classList.toggle("none");
+}
+ 
+boton.addEventListener("click", none);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
